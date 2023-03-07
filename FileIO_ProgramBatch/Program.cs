5051ecc2da51f58bb6918af70c2e4a9e8db904c2
Snehal979 <bansodsnehal535@gmail.com>
2023-03-08 01:41:22 +0530

    [snehal]Add: main method

---
+++ FileIO_ProgramBatch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIO_ProgramBatch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("File IO and Stream");
+        }
+    }
+}
